984905e7deacff0948769c061ae8f3706d95f21b
Brayan Josue Medina <brayanmedina35@yahoo.com>
2018-04-30 02:38:28 -0600

    He agregado funcionalidad a los estadisticos que genera el algoritmo ExpectiMax

--- Simulado 2048/scripts/config.js
+++ Simulado 2048/scripts/config.js
@@ -1,8 +1,18 @@
 'use strict';
 
 /*Agremos todos los modulos a utilizar*/
-angular.module('2048Simulator', ['ui.router', 'ngAnimate', 'toastr', 'chart.js'])
+angular.module('2048Simulator', ['ui.router', 'ngAnimate', 'toastr', 'chart.js', 'firebase'])
 .config(['ChartJsProvider','toastrConfig', '$stateProvider', '$urlRouterProvider', function(ChartJsProvider, toastrConfig, $stateProvider, $urlRouterProvider) {
+		// Initialize Firebase
+		var config = {
+			apiKey: "AIzaSyCeP-a1xQRp4zU5J1POkwwOVvyW1JmU0tM",
+			authDomain: "game-c5bab.firebaseapp.com",
+			databaseURL: "https://game-c5bab.firebaseio.com",
+			projectId: "game-c5bab",
+			storageBucket: "game-c5bab.appspot.com",
+			messagingSenderId: "1082425832460"
+		};
+		firebase.initializeApp(config);
 		$stateProvider 
 			/*Establecemos los estados por cada vista y sub-vista de la aplicacion*/
 			.state('principal', {
--- Simulado 2048/scripts/controllers/estadisticos/estadisticosCtrl.js
+++ Simulado 2048/scripts/controllers/estadisticos/estadisticosCtrl.js
@@ -1,17 +1,36 @@
 'use strict';
 angular.module('2048Simulator')
-.controller('estadisticosCtrl', ['$scope', '$location', function($scope, $location){
+.controller('estadisticosCtrl', ['$scope', '$EstadisticosService', function($scope, $EstadisticosService){
+	$scope.dataGrafico1 = [[],[]];
+	$scope.dataGrafico2 = [[]];
+	$scope.labelsGrafico1 = ['Heuristica 1', 'Heuristica 2', 'Heuristica 3', 'Heuristica 4'];
+	$scope.seriesGrafico1 = ['Porcentaje ganado ', 'Porcentaje perdido '];
 	
-	
-	$scope.labels = ['Heuristica 1', 'Heuristica 2', 'Heuristica 3', 'Heuristica 4'];
-	$scope.series = ['Porcentaje ganado ', 'Porcentaje perdido '];
+	$scope.estadisticosExpectiMax = $EstadisticosService.obtenerEstadisticosExpectiMax();
+	$scope.estadisticosExpectiMax.$loaded()
+	.then(function(respuesta) {
+		//Obtenemos los valores de las partidas ganadas por cada heuristica
+		Object.keys(respuesta.heuristicas.partidasGanadas).map(function(objectKey, index) {
+		    $scope.dataGrafico1[0].push(respuesta.heuristicas.partidasGanadas[objectKey].porcentaje);
+		});
 
-	$scope.data = [
-		[65, 59, 80, 81],
-		[28, 48, 40, 19]
-	];
+		//Obtenemos los valores de las partidas perdidas por cada heuristica
+		Object.keys(respuesta.heuristicas.partidasPerdidas).map(function(objectKey, index) {
+		    $scope.dataGrafico1[1].push(respuesta.heuristicas.partidasPerdidas[objectKey].porcentaje);
+		});
+
+		//Obtenemos los valores de movimientos para obtener las potencias de 2
+		$scope.dataGrafico2 = [[respuesta.potencias.p8, 
+								respuesta.potencias.p16, 
+								respuesta.potencias.p32,
+								respuesta.potencias.p64, 
+								respuesta.potencias.p128, 
+								respuesta.potencias.p256,
+								respuesta.potencias.p512, 
+								respuesta.potencias.p1024,
+								respuesta.potencias.p2048]];
+     });
 
 	$scope.labelsGrafico2 = ["8", "16", "32", "64", "128", "256", "512", "1024", "2048"];
-	$scope.seriesGrafico2 = ['Series A'];
-	$scope.dataGrafico2 = [[65, 59, 80, 81, 56, 55, 40, 1, 85]];
+	$scope.seriesGrafico2 = ['Promedio movimientos: '];
 }])
